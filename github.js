90f7d318d0009e374091758153eeebc575ef93f2
Dan Poggi <dan.poggi@detroitlabs.com>
2016-01-13 13:22:39 -0500

    De-jquery'd github.js

--- github.js
+++ github.js
@@ -5,28 +5,29 @@
 // This software may be modified and distributed under the terms
 // of the MIT license. See the LICENSE file for details.
 
-(function () {
+(function() {
+  var org, substr, els;
   if (window.location.host.indexOf("github.com") === -1) return;
 
-  $("li.js-subscription-row").each(function (idx, li) {
-    var $li, slug, tokens, user, repo, $btn, org, substr;
+  org = prompt("Organization:");
+  substr = prompt("Substring:");
 
-    $li = $(li);
-    slug = $($li.find("a")[0]).text().trim();
+  els = [].slice.call(document.querySelectorAll("li.js-subscription-row"));
+  els.forEach(function(el) {
+    var slug, tokens, user, repo, btn;
+
+    slug = el.querySelector("a").textContent.trim();
     tokens = slug.split("/");
-    user = tokens[0];
+    user = tokens[0]
     repo = tokens[1];
-    $btn = $($li.find("form.js-unsubscribe-form button")[0]);
+    btn = el.querySelector("form.js-unsubscribe-form button");
 
     // Will prompt to unsubscribe from repos not from org organization, or
     // ones that are from org organization but contain substr substring.
-    org = prompt("Organization:");
-    substr = prompt("Substring:");
-
     if (user.indexOf(org) === -1 ||
         repo.toLowerCase().indexOf(substr) !== -1) {
       if (confirm("Does you wanna unsubscribe " + slug + "???")) {
-        $btn.click();
+        btn.click();
       }
     }
   });
