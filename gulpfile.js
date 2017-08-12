e33964fb3e75d290a2dfe61134a10501ec94bc8a
Dan Poggi <dan.poggi@gmail.com>
2017-08-12 17:09:32 -0400

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,32 @@
 "use strict";
 
-const gulp = require("gulp"),
-      changed = require("gulp-changed"),
-      prettydiff = require("gulp-prettydiff"),
-      inject = require("gulp-inject-string"),
-      jscs = require("gulp-jscs"),
-      rimraf = require("gulp-rimraf");
+const gulp = require("gulp");
+const changed = require("gulp-changed");
+const prettydiff = require("gulp-prettydiff");
+const inject = require("gulp-inject-string");
+const jscs = require("gulp-jscs");
+const rimraf = require("gulp-rimraf");
 
-const paths = {
-  src: "src/**/*.js",
-  build: "build",
-};
+const sourcesGlob = "src/**/*.js";
+const buildDir = "build";
 
-gulp.task("default", () => {
-  return gulp.src(paths.src)
-    .pipe(changed(paths.build))
-    .pipe(prettydiff({
-      lang: "javascript",
-      mode: "minify",
-    }))
-    .pipe(inject.prepend("javascript:"))
-    .pipe(gulp.dest(paths.build));
+gulp.task("clean", () => {
+  return gulp.src(buildDir, { read: false })
+      .pipe(rimraf());
 });
 
 gulp.task("lint", () => {
-  return gulp.src(paths.src)
-    .pipe(jscs())
-    .pipe(jscs.reporter());
+  return gulp.src(sourcesGlob)
+      .pipe(jscs())
+      .pipe(jscs.reporter());
 });
 
-gulp.task("clean", () => {
-  return gulp.src(paths.build, { read: false })
-    .pipe(rimraf());
+gulp.task("build", () => {
+  return gulp.src(sourcesGlob)
+      .pipe(changed(buildDir))
+      .pipe(prettydiff({ lang: "javascript", mode: "minify" }))
+      .pipe(inject.prepend("javascript:"))
+      .pipe(gulp.dest(buildDir));
 });
+
+gulp.task("default", ["build"]);
