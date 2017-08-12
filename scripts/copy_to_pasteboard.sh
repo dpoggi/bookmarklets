89a5d3c75f005fe1060dc855f43a07aa2945e5ab
Dan Poggi <dan.poggi@gmail.com>
2017-08-12 17:05:48 -0400

    Update scripts

--- scripts/copy_to_pasteboard.sh
+++ scripts/copy_to_pasteboard.sh
@@ -2,24 +2,30 @@
 #
 # copy_to_pasteboard.sh -- cross-platform script to pasteboard a file or stdin
 #
-# Copyright (C) 2015 Dan Poggi
+# Copyright (C) 2017 Dan Poggi
 #
 # This software may be modified and distributed under the terms
 # of the MIT license. See the LICENSE file for details.
 
-set -e
+set -eo pipefail
 
-if hash pbcopy 2> /dev/null; then
-  CMD=( pbcopy )
-elif hash xsel 2> /dev/null; then
-  CMD=( xsel --pasteboard --input )
-else
-  printf >&2 "Error: need pbcopy or xsel to copy to pasteboard.\n"
-  exit 1
-fi
+main() {
+  local -a cmd
 
-if [[ -s "$1" ]]; then
-  cat "$1" | ${CMD[*]}
-else
-  ${CMD[*]}
-fi
+  if hash pbcopy 2>/dev/null; then
+    cmd=(pbcopy)
+  elif hash xsel 2>/dev/null; then
+    cmd=(xsel --pasteboard --input)
+  else
+    printf >&2 "Error: neither pbcopy nor xsel found\n"
+    return 1
+  fi
+
+  if [[ -r "$1" ]]; then
+    cat "$1" | "${cmd[@]}"
+  else
+    "${cmd[@]}"
+  fi
+}
+
+main "$@"
--- scripts/get_champ_ids.rb
+++ scripts/get_champ_ids.rb
@@ -2,7 +2,7 @@
 #
 # get_champ_ids.rb -- scrape Mobafire for champion name -> ID mapping
 #
-# Copyright (C) 2015 Dan Poggi
+# Copyright (C) 2017 Dan Poggi
 #
 # This software may be modified and distributed under the terms
 # of the MIT license. See the LICENSE file for details.
@@ -10,15 +10,16 @@
 require 'net/http'
 require 'json'
 
-data = Net::HTTP.get('www.mobafire.com', '/league-of-legends/champions')
-regexp = /\/champion\/([a-z\-]*)-(\d{1,3})/
-matches = data.scan(regexp).map do |match|
+html_blob = Net::HTTP.get('www.mobafire.com', '/league-of-legends/champions')
+
+matches = html_blob.scan(/\/champion\/([a-z\-]*)-(\d{1,3})/).map do |match|
   [match[0].gsub('-', ' '), match[1].to_i]
 end
 
-$stdout.write(JSON.pretty_generate(Hash[matches]))
-if $stdout.tty?
-  $stderr.puts "\n\nSuccess! Pipe to scripts/copy_to_pasteboard.sh to copy champion IDs as JSON."
+STDOUT.write(JSON.pretty_generate(Hash[matches]))
+
+if STDOUT.tty?
+  STDERR.puts "\n\nSuccess! Pipe to scripts/copy_to_pasteboard.sh to copy champion IDs as JSON."
 else
-  $stderr.puts "Success! Check the clipboard!"
+  STDERR.puts "Success! Check the clipboard!"
 end
