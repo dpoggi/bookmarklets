c6c85b09a1a0ba337724489cb8451c1b68ef8a6b
Dan Poggi <dan.poggi@gmail.com>
2023-02-17 21:14:42 -0500

    Lessen gulpfile cringe

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,37 @@
 "use strict";
 
 const gulp = require("gulp");
+const { dest } = gulp;
 const prettydiff = require("gulp-prettydiff");
-const injectString = require("gulp-inject-string");
+const { append, prepend } = require("gulp-inject-string");
 const rename = require("gulp-rename");
 const rimraf = require("gulp-rimraf");
 
 const sourcesGlob = "src/**/*.js";
 const buildDir = "build";
 
-const clean = exports.clean = () => {
+exports.clean = () => {
   return gulp
     .src(buildDir, { read: false })
     .pipe(rimraf());
 };
 
-const build = exports.build = () => {
+exports.build = () => {
   return gulp
     .src(sourcesGlob)
     .pipe(prettydiff({ lang: "javascript", mode: "minify" }))
-    .pipe(injectString.prepend("javascript:"))
-    .pipe(gulp.dest(buildDir));
+    .pipe(prepend("javascript:"))
+    .pipe(dest(buildDir));
 };
 
-const buildHTML = exports.buildHTML = () => {
+exports.buildHTML = () => {
   return gulp
     .src(sourcesGlob)
     .pipe(prettydiff({ lang: "javascript", mode: "minify" }))
-    .pipe(injectString.prepend("<script defer>\"use strict\";"))
-    .pipe(injectString.append("</script>"))
+    .pipe(prepend("<script defer>\"use strict\";"))
+    .pipe(append("</script>"))
     .pipe(rename({ extname: ".html" }))
-    .pipe(gulp.dest(buildDir));
+    .pipe(dest(buildDir));
 };
 
-exports.default = build;
+exports.default = exports.build;
