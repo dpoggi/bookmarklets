48f5b33f7c6a262f3356bf5793af75d04d0d0221
Dan Poggi <dan.poggi@gmail.com>
2023-02-17 20:39:51 -0500

    Add Gulp task buildHTML

--- gulpfile.js
+++ gulpfile.js
@@ -4,22 +4,36 @@ const gulp = require("gulp");
 const changed = require("gulp-changed");
 const prettydiff = require("gulp-prettydiff");
 const injectString = require("gulp-inject-string");
+const rename = require("gulp-rename");
 const rimraf = require("gulp-rimraf");
 
 const sourcesGlob = "src/**/*.js";
 const buildDir = "build";
 
-exports.clean = function() {
-  return gulp.src(buildDir, { read: false })
-      .pipe(rimraf());
+const clean = exports.clean = () => {
+  return gulp
+    .src(buildDir, { read: false })
+    .pipe(rimraf());
 };
 
-exports.build = function() {
-  return gulp.src(sourcesGlob)
-      .pipe(changed(buildDir))
-      .pipe(prettydiff({ lang: "javascript", mode: "minify" }))
-      .pipe(injectString.prepend("javascript:"))
-      .pipe(gulp.dest(buildDir));
+const build = exports.build = () => {
+  return gulp
+    .src(sourcesGlob)
+    .pipe(changed(buildDir))
+    .pipe(prettydiff({ lang: "javascript", mode: "minify" }))
+    .pipe(injectString.prepend("javascript:"))
+    .pipe(gulp.dest(buildDir));
 };
 
-exports.default = exports.build;
+const buildHTML = exports.buildHTML = () => {
+  return gulp
+    .src(sourcesGlob)
+    .pipe(changed(buildDir))
+    .pipe(prettydiff({ lang: "javascript", mode: "minify" }))
+    .pipe(injectString.prepend("<script defer>\"use strict\";"))
+    .pipe(injectString.append("</script>\n"))
+    .pipe(rename({ extname: ".html" }))
+    .pipe(gulp.dest(buildDir));
+};
+
+exports.default = build;
