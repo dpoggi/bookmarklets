36f2ee7c0a72dea134020cc6055a44dc428d16e6
Dan Poggi <dan.poggi@gmail.com>
2023-02-17 20:39:46 -0500

    Resurrect gulp build

--- gulpfile.js
+++ gulpfile.js
@@ -4,29 +4,22 @@ const gulp = require("gulp");
 const changed = require("gulp-changed");
 const prettydiff = require("gulp-prettydiff");
 const injectString = require("gulp-inject-string");
-const jscs = require("gulp-jscs");
 const rimraf = require("gulp-rimraf");
 
 const sourcesGlob = "src/**/*.js";
 const buildDir = "build";
 
-gulp.task("clean", () => {
+exports.clean = function() {
   return gulp.src(buildDir, { read: false })
       .pipe(rimraf());
-});
+};
 
-gulp.task("lint", () => {
-  return gulp.src(sourcesGlob)
-      .pipe(jscs())
-      .pipe(jscs.reporter());
-});
-
-gulp.task("build", ["lint"], () => {
+exports.build = function() {
   return gulp.src(sourcesGlob)
       .pipe(changed(buildDir))
       .pipe(prettydiff({ lang: "javascript", mode: "minify" }))
       .pipe(injectString.prepend("javascript:"))
       .pipe(gulp.dest(buildDir));
-});
+};
 
-gulp.task("default", ["build"]);
+exports.default = exports.build;
